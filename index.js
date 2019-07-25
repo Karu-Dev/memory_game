64183f83d22aac54becac5e9b755360d631882d1
RealRemeku <karliszepkans@gmail.com>
2019-07-25 18:28:13 +0300

    Created multiple fuctions to create an array

---
+++ index.js
@@ -0,0 +1,86 @@
+let object
+
+
+let size=4;
+
+function genAvatars(size){
+    let allAvatars=[]
+    let uniqueAvatarCount=size*size/2
+    for(let i=0; i<2; i++){
+        for(let j=0; j<uniqueAvatarCount; j++){
+            allAvatars.push(j)
+        }
+    }
+    return allAvatars;
+
+}
+let avatars=genAvatars(size)
+console.log(avatars)
+
+
+function genEmptyField1D(size){
+    let field = []
+    for(let i=0; i<size*size; i++){
+        let cell={
+            isOpen: false,
+            isFound: false,
+            avatarIndex: 0
+        }
+        field.push(cell)
+    }
+    return field;
+}
+
+
+let field = genEmptyField1D(size)
+
+
+function fillWithAvatars1d(field, avatars){
+    for(let i = 0 ; i<size*size; i++){
+        let avatarIndex = Math.floor(Math.random()*avatars.length)
+        field[i].avatarIndex=avatars[avatarIndex]
+        avatars.splice(avatarIndex,1)
+    }
+}
+
+
+
+fillWithAvatars1d(field,avatars)
+
+function drawField(){
+    let container= document.createElement("div")
+    container.className="container"
+}
+
+
+console.log(field)
+// function generateEmptyField(size) {
+//     let field = []
+
+//     for (let y = 0; y < size; y++) {
+//         let row = []
+//         for (let x = 0; x < size; x++) {
+//             let cell = {
+//                 x,
+//                 y,
+//                 isOpen: false,
+//                 isFound: false,
+//                 avatarIndex: 0
+//             }
+//             row.push(cell)
+//         }
+//         field.push(row)
+//     }
+//     return field;
+// }
+    // function fillWithAvatars(field, avatars) {
+        //     for (let x in field) {
+    //         let row = field[x];
+    //         for (let y in row) {
+        //             let cell = row[y]
+        //             let avatarIndex = Math.floor(Math.random()*avatars.length)
+    //             cell.avatarIndex=avatars[avatarIndex]
+    //             avatars.splice(avatarIndex,1)
+    //             }
+    //         }
+    //     }
